a4f5f1938bf80839fc708fb01ec8f5e69ab4dd67
Nghi Tran <nghi.t.tran8@gmail.com>
2020-03-05 16:27:32 -0800

    updates

--- ourArray.js
+++ ourArray.js
@@ -128,7 +128,30 @@ main();
 // What is the purpose of the _resize() function in your Array class?
 //   Resize function add to the available capacity by copying and pasting old blocks and free up values to create the capacity
 
-// #5)
+/**
+ * #5
+ A common mistake users make when they type in an URL is to put spaces between words or letters. 
+ One solution that developers can use to solve this problem is to replace any spaces with a '%20'. 
+ Write a method that takes in a string and replaces all its empty spaces with a '%20'. 
+ Your algorithm can only make 1 pass through the string.
+
+ Alternative solution:
+ function urlifyString(str){
+    let spaceCount = 0;
+    let index = 0;
+    let newString = '';
+    for(let i=0; i<str.length; i++){
+        if(str[i]===' '){
+            newString+='%20'
+ 
+        }else{
+            newString += str[i];
+        }
+        
+    }
+    return newString;
+}
+ */
 
 const URLify = str => {
   let newStr = str; //constant O(1)
@@ -141,8 +164,12 @@ const URLify = str => {
 
 // console.log(URLify('www.thinkful.com /tauh ida parv een'));
 
-// #6)
-//Imagine you have an array of numbers. Write an algorithm to remove all numbers less than 5 from the array.
+/**
+ * #6
+  Imagine you have an array of numbers. Write an algorithm to remove all numbers 
+  less than five from the array. 
+  Don't use array's built-in `.filter` method here; write the algorithm from scratch.
+ */
 
 const filterArray = arr => {
   //every element needs to be checked, no matter size of array, so O(n) linear time complexity
@@ -156,27 +183,55 @@ const filterArray = arr => {
 
 // console.log(filterArray([1,3,6,7,8]));
 
-// #7
-// You are given an array containing positive and negative integers.
-// Write an algorithm which will find the largest sum in a continuous sequence.
+/**
+ * #7
+ You are given an array containing positive and negative integers.
+ Write an algorithm which will find the largest sum in a continuous sequence.
+ */
 
 const maxSubArray = arr => {
-  sum = 0;
-  max = 0;
+  let sum = 0;
+  let max = 0;
   for (let i = 0; i < arr.length; i++) {
     // O(n) depends on size of array and runtime increase proportionally
-    sum += arr[i];
-    if (sum > max) {
-      max = sum;
-    }
+    // Set sum to math.max()
+    let item = arr[i];
+    // To avoid negative
+    sum = Math.max(0, sum + item);
+    max = Math.max(max, sum);
+    // console.log(item, sum, max);
   }
   return max;
 };
 
-// console.log(maxSubArray([4, 6, -3, 5, -2, 1]));
+// console.log(maxSubArray([-3, 4, 6, -3, 5, -2, 1]));
 
-// #8
-// Imagine you have 2 arrays which have already been sorted. Write an algorithm to merge the 2 arrays into a single array, which should also be sorted.
+/**
+ * #8
+ Imagine you have two arrays which have already been sorted. 
+ Write an algorithm to merge the two arrays into a single array, 
+ which should also be sorted. 
+
+ Alternative solution:
+ function merge_arrays(arr1, arr2) {
+	let idx1 = 0, idx2 = 0;
+	let ret = [];
+	while (idx1 < arr1.length && idx2 < arr2.length) {
+		if (arr1[idx1] <= arr2[idx2])
+			ret.push(arr1[idx1++]);
+		else
+			ret.push(arr2[idx2++]);
+	}
+	// One array is now empty. Join the rest of the other array on.
+	if (idx2 < arr2.length) {
+		idx1 = idx2;
+		arr1 = arr2;
+	}
+	while (idx1 < arr1.length) 
+        ret.push(arr1[idx1++]);
+	return ret;
+}
+*/
 
 const mergeArray = (arr1, arr2) => {
   let arr3 = arr1.concat(arr2); // O(1)
@@ -186,12 +241,21 @@ const mergeArray = (arr1, arr2) => {
 
 // console.log(mergeArray([1, 3, 6, 8, 11], [2, 3, 5, 8, 9, 10]));
 
-//#9 Write an algorithm that deletes given characters from a string.
+/**
+ * #5
+ Write an algorithm that deletes given characters from a string. For example, given a 
+ string of "Battle of the Vowels: Hawaii vs. Grozny" and characters to be removed are 
+ "aeiou", the algorithm should transform the original string to "Bttl f th Vwls: Hw vs. Grzny". 
+ Do not use Javascript's filter, split, or join methods.
 
-// Input:'Battle of the Vowels: Hawaii vs. Grozny', 'aeiou'
-// Output: 'Bttl f th Vwls: Hw vs. Grzny'
-
-// Do not use Javascript's filter, split, or join methods.
+ function vowels(str, specifiedChars) {
+   // global insensitive - meaning search through the entire string and case insensitive
+   const regex = '/[{specifiedChars}]/gi'; 
+   // match() return an array with the characters in it
+   const matches = str.match(new Regex(regex));
+   return matches ? matches.length : 0;
+ }
+ */
 
 const deleteChars = (string, specifiedChars) => {
   let newStr = string;
@@ -218,49 +282,72 @@ const deleteChars = (string, specifiedChars) => {
 //   ])
 // );
 
-//#10
-// Given an array of numbers, write an algorithm to find out the products of every other number except the number at each index.
+/**
+ * #10
+ Given an array of numbers, write an algorithm to find out the products of every number, 
+ except the one at that index.
+ Input:[1, 3, 9, 4]
+ Output:[108, 36, 12, 27]
 
-// Input:[1, 3, 9, 4]
-// Output:[108, 36, 12, 27]
+ Alternative solution:
+ const products = arr => {
+  // Could use reduce to multiply everything
+  const product = arr.reduce((a, b) => a * b);
+  output = arr.map(i => product / i);
+  return output;
+};
+ */
 
 const products = arr => {
   let output = []; // O(1)
   let product = 1; // O(1)
-  // Could use reduce to multiply everything
-  product = arr.reduce((a, b) => a * b);
-  // for (let i = 0; i < arr.length; i++) {
-  //   // O(n)
-  //   // 1 * 1 = 1 // 3*1 = 3
-  //   product *= arr[i]; //multiplying everything
-  // }
-  //by the end of the first for loop, product is = 108.
-  output = arr.map(i => product / i);
-  // for (let i = 0; i < arr.length; i++) {
-  //   // O(n)
-  //   output.push(product / arr[i]); //108/1...108/3
-  // }
+  for (let i = 0; i < arr.length; i++) {
+    // O(n)
+    // 1 * 1 = 1 // 3*1 = 3
+    product *= arr[i]; //multiplying everything
+  }
+  // by the end of the first for loop, product is = 108.
+  for (let i = 0; i < arr.length; i++) {
+    // O(n)
+    output.push(product / arr[i]); //108/1...108/3
+  }
   return output;
 };
 
-console.log(products([1, 3, 9, 4]));
+// console.log(products([1, 3, 9, 4]));
 
-//#11
-// Write an algorithm which searches through a 2D array, and whenever it finds a 0 should set the entire row and column to 0.
+/**
+ * #11
+ Write an algorithm which searches through a 2D array, and whenever it finds a zero should 
+ set the entire row and column to zero.
+ Nested for loops = quadratic time - could be improved
+ Map object - alternative
+ */
 
 const replace0 = arr => {
-  marker = [
-    [x, y],
-    [x, y]
-  ];
+  // Rows and columns with 0s
+  const rows = [];
+  const cols = [];
+  // Find rows and columns with 0s
   for (let i = 0; i < arr.length; i++) {
     // O(n)
+    // Skip find with rows has no 0s
     if (arr[i].includes(0)) {
       for (let j = 0; j < arr[i].length; j++) {
         // O(n)
         if (arr[i][j] === 0) {
-          marker.push([i, j]);
+          rows[i] = true;
+          cols[i] = true;
         }
+      }
+    }
+  }
+
+  // Insert 0 for if row or column has 0s
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (row[i] || cols[j]) {
+        arr[i][j] = 0;
       }
     }
   }
@@ -277,30 +364,15 @@ const input = [
 
 // console.log(replace0(input));
 
-// #12
-// Given 2 strings, str1 and str2, write a program that checks if str2 is a rotation of str1.
+/**
+ * #12
+ Given two strings, str1 and str2, write a program that checks if str2 is a rotation of str1.
+ Usually use a hashmap to avoid nested for loops - avoid quadratic time
+ */
 
 const rotation = (str1, str2) => {
-  // Find the indices of str2 that contains the first character of str1
-  // Rotate it enough to get get a match
-  const numOfChar = str2.match(new RegExp(str1[0]) || []).length;
-  let found = "";
-  for (let i = 0; i < str1; i++) {
-    found = "";
-    if (str2[i] === str1[0]) {
-      found += str2[i];
-      for (let j = i + 1; j < array.length; j++) {
-        if (j < str1.length - 1) {
-          found += str2[j];
-        } else {
-        }
-      }
-      if (found === str1) {
-        return true;
-      }
-    }
-  }
-  return false;
+  // Key is to concat string 2
+  return (str2 + str2).indexOf(st1) !== -1; // boolean value
 };
 
 console.log(rotation("amazon", "azonma")); // false
